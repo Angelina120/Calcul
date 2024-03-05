ed506c337907cfee03c667182ec2a47c6784e18f
Angelina120 <mail_reserve_ru@mail.ru>
2024-03-05 10:37:56 +0300

    feat: + класс

--- calculator/src/main/java/ru/krista/example/App.java
+++ calculator/src/main/java/ru/krista/example/App.java
@@ -2,11 +2,19 @@ package ru.krista.example;
 
 import java.util.Scanner;
 
+
 public class App {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String input = in.nextLine();
         String[] parts = input.split("");
+        int a,b;
+        try {
+            a = Integer.parseInt(parts[0]);
+            b = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException exception) {
+            System.out.println("Ошибка ввода!");
+        }
 
     }
 }
